ffb65c2096a117c7fe0ceaa73fa41601bf863b49
pai peng <paipeng@192.168.1.15>
2022-02-14 10:59:04 -0500

    #16 created tables for item, customer, and shopping_cart

--- backend/src/main/resources/db.sql
+++ backend/src/main/resources/db.sql
@@ -1,1 +1,34 @@
-## test
+CREATE
+DATABASE shoppingweb;
+CREATE TABLE customer
+(
+    id         integer     NOT NULL,
+    username   VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name  VARCHAR(50) NOT NULL,
+    password   VARCHAR(50) NOT NULL,
+    email      VARCHAR(150)
+
+);
+
+CREATE TABLE item
+(
+    id              INTEGER PRIMARY KEY,
+    name            VARCHAR(200)     NOT NULL,
+    price           DOUBLE PRECISION NOT NULL,
+    brand           VARCHAR(70),
+    description     VARCHAR(5000),
+    rate            DOUBLE PRECISION,
+    in_stock_status BOOLEAN          NOT NULL,
+    thumbnail       VARCHAR(5000),
+    quantity        INTEGER
+);
+
+CREATE TABLE shopping_cart
+(
+    id    integer          NOT NULL,
+    name  VARCHAR(50),
+    price double precision NOT NULL,
+    brand VARCHAR(70)
+
+);
