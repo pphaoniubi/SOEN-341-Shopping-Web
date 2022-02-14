b5a36c41ae608b29a73ae7b59e47e34c6f46e881
paullmq8 <paullmq8@gmail.com>
2022-02-14 15:58:24 -0500

    #29 #31 create one api for frontend to query the detailed information of one item by their id.

--- backend/src/main/java/com/amazon/controller/CustomerItemController.java
+++ backend/src/main/java/com/amazon/controller/CustomerItemController.java
@@ -1,18 +1,25 @@
 package com.amazon.controller;
 
+import com.amazon.dto.ItemDetailDto;
 import com.amazon.dto.ItemListDto;
 import com.amazon.entity.Item;
+import com.amazon.mapper.ItemDetailMapper;
 import com.amazon.mapper.ItemListMapper;
 import com.amazon.service.CustomerItemService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/customer/item")
@@ -29,6 +36,18 @@ public class CustomerItemController {
         Page<Item> itemPage = customerItemService.findAllItems(pageable);
         List<ItemListDto> itemListDtos = ItemListMapper.INSTANCE.map(itemPage.getContent());
         return new PageImpl<>(itemListDtos, pageable, itemPage.getTotalElements());
+    }
+
+    @GetMapping("/{id}")
+    public ItemDetailDto findItemById(@PathVariable("id") int id,
+                                      HttpServletResponse response) throws IOException {
+        Optional<Item> itemOptional = customerItemService.findOneById(id);
+        if (itemOptional.isPresent()) {
+            Item item = itemOptional.get();
+            return ItemDetailMapper.INSTANCE.map(item);
+        }
+        response.sendError(HttpStatus.NOT_FOUND.value(), "There's no such item.");
+        return null;
     }
 
     public List<Item> findAllByname(String name){
---
+++ backend/src/main/java/com/amazon/dto/ItemDetailDto.java
@@ -0,0 +1,77 @@
+package com.amazon.dto;
+
+public class ItemDetailDto {
+
+    private int id;
+    private String name;
+    private double price;
+    private String brand;
+    private String description;
+    private double rate;
+    private String thumbnail;
+    private int quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
