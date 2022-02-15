a6cdf7076c5b06fedc9fe52ae9db452b1133680e
paullmq8 <paullmq8@gmail.com>
2022-02-14 19:52:41 -0500

    #36 The api for searching items by name should only return for sale items. And it should search similar name items.

--- backend/src/main/java/com/amazon/repository/CustomerItemRepository.java
+++ backend/src/main/java/com/amazon/repository/CustomerItemRepository.java
@@ -6,8 +6,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerItemRepository extends JpaRepository<Item, Integer> {
+
     Page<Item> findAllByForSaleTrue(Pageable pageable);
     Item findOneByIdAndForSaleTrue(int id);
-    Page<Item> findAllByName(String name, Pageable pageable);
+    Page<Item> findAllByNameContainingIgnoreCaseAndForSaleTrue(String name, Pageable pageable);
     Page<Item> findAllByBrand(String brand, Pageable pageable);
 }
