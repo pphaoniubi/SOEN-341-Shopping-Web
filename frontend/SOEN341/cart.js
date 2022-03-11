ee30c4b0a3f6bc822ca1270bfc3afa61f54a8ad3
Pai Peng <pphaoniubi@gmail.com>
2022-03-12 01:54:09 +0800

    pushing some shopping cart frontend code for testing

--- frontend/SOEN341/cart.js
+++ frontend/SOEN341/cart.js
@@ -7,30 +7,32 @@ for(var i=0;i<removecarteButtoms.length;i++)
     var button=removecarteButtoms[i]
     button.addEventListener('click',function(event){
         var removebuttoncliked=event.target
-        removebuttoncliked.parentElement.parentElement.remove()
-        //removebuttoncliked.closest('tr').remove()   
+        removebuttoncliked.parentElement.parentElement.parentElement.parentElement.remove()
+        //removebuttoncliked.closest('tr').remove()  
         updateCartTotal() 
     });
 }
 
-function updataCarteTotal(){
-    var cartItems=document.getElementsByClassName("cart-items")[0]
-    var cartRows = cartPage.getElementsByClassName('cart-info')
+function updateCartTotal(){
+    var cartItems = document.getElementsByClassName('small-container cart-page')[0]
+    var cartRows = cartItems.getElementsByClassName('item-info')
     console.log(cartItems)
     
     var total = 0
     for (var i = 0; i < cartRows.length; i++) {
-        var cartitems = cartItems[i]
-        var priceElement = cartitems.getElementsByClassName('cart-price')[0]
-        var quantityElement = cartitems.getElementsByClassName('cart-quantity-input')[0]
-        
+        var cartRow = cartRows[i]
+        var quantity = cartItems.getElementsByClassName('cart-quantity-input')[0]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        //var quantityElement = cartQuantity.getElementsByClassName('cart-quantity-input')[0] //non-existent
+        quantity = quantity.value
+        console.log(quantity)
         var price = parseFloat(priceElement.innerText.replace('$', ''))
-        var quantity = quantityElement.value
+        console.log(price)
         total = total + (price * quantity)
     }
     total = Math.round(total * 100) / 100
-    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
-    
+    console.log(total)
+    document.getElementsByClassName('total-price')[0].innerText = '$' + total
 
 }
 
