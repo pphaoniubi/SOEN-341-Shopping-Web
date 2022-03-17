e472e640fb0d3c8c9968b894e982b03a8a947633
andylee511 <65518716+andylee511@users.noreply.github.com>
2022-03-17 00:03:49 -0400

    fixing bug for pressing login and log out

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -8,13 +8,24 @@ $(window).on('load', function() {
     if(user != null){
         //user is logged in
         $(".account-info").html(`Welcome! ${JSON.parse(user).email}`);
+        $("#log-btn").on('click', function() {
+            logout();
+        })
     }else{
-
+        $(".account-info").html(`login in place order`);
+        $("#log-btn").html('log in')
+        $("#log-btn").on('click', function() {
+            login();
+        })
     }
 })
 function logout() {
+
     sessionStorage.clear;
     sessionStorage.setItem("toLogin", "1")
 
+    window.location.href = "login.html";
+}
+function login(){
     window.location.href = "login.html";
 }
